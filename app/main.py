0ec557613c218794646eb91f5cc063b1893ad52b
Vovas <vovaafon25@gmail.com>
2025-06-20 09:30:42 +0300

    improved the code by gleb`s and ai advices

--- app/main.py
+++ app/main.py
@@ -22,28 +22,17 @@ class CarWashStation:
         self.count_of_ratings = count_of_ratings
 
     def calculate_washing_price(self, car: Car) -> float:
-        overall_cost = ((((car.comfort_class
+        return round(((((car.comfort_class
                           * (self.clean_power - car.clean_mark))
                          * self.average_rating))
-                        / self.distance_from_city_center)
+                        / self.distance_from_city_center),1)
 
-        overall_cost = round(overall_cost, 1)
-
-        return overall_cost
-
-    def serve_cars(self, cars: list) -> None:
-
-        income = 0
-
-        for car in cars:
-            if self.clean_power > car.clean_mark:
-                income += self.calculate_washing_price(car)
-                self.wash_single_car(car)
-        return income
+    def serve_cars(self, cars: list) -> float:
+        return sum([self.calculate_washing_price(car) for car in cars])
 
     def wash_single_car(self, car: Car) -> None:
-
-        car.clean_mark = self.clean_power
+        if self.clean_power > car.clean_mark:
+            car.clean_mark = self.clean_power
 
     def rate_service(self, mark: float) -> None:
 
