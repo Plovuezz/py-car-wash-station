1996b77f1bc904e117b610215067bd95ebbb0781
Vovas <vovaafon25@gmail.com>
2025-06-20 09:41:35 +0300

    fixed flake8 errors

--- app/main.py
+++ app/main.py
@@ -23,9 +23,9 @@ class CarWashStation:
 
     def calculate_washing_price(self, car: Car) -> float:
         return round(((((car.comfort_class
-                          * (self.clean_power - car.clean_mark))
-                         * self.average_rating))
-                        / self.distance_from_city_center),1)
+                        * (self.clean_power - car.clean_mark))
+                        * self.average_rating))
+                      / self.distance_from_city_center), 1)
 
     def serve_cars(self, cars: list) -> float:
         return sum([self.calculate_washing_price(car) for car in cars])
